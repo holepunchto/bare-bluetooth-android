3466f80942e64e148b765450906701b418cade9f
Tony Gorez <gorez.tony@gmail.com>
2026-06-22 18:22:32 +0200

    fix(lib): types
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- lib/central.d.ts
+++ lib/central.d.ts
@@ -6,7 +6,7 @@ export type BluetoothState = 'off' | 'turningOn' | 'on' | 'turningOff'
 export interface CentralEventMap extends EventMap {
   stateChange: [state: BluetoothState]
   discover: [peripheral: Peripheral]
-  connect: [peripheral: Peripheral, error?: string]
+  connect: [peripheral: Peripheral]
   disconnect: [peripheral: Peripheral | null, error?: string]
   connectFail: [id: string, error: string]
   scanFail: [errorCode: number]
